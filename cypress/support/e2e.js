e79267b0655ef3c2754d1721b22e36f968569a3e
Oleksandr Tromsa <agrimouse@gmail.com>
2025-06-08 23:41:09 +0300

    initial commit

---
+++ cypress/support/e2e.js
@@ -0,0 +1,9 @@
+describe('Login Test', () => {
+  it('should log in to demo site', () => {
+    cy.visit('https://opensource-demo.orangehrmlive.com/')
+    cy.get('input[name="username"]').type('Admin')
+    cy.get('input[name="password"]').type('admin123')
+    cy.get('button[type="submit"]').click()
+    cy.url().should('include', '/dashboard')
+  })
+})
